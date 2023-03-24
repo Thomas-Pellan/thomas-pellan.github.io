952517ebeb7027add3a03fa744c9ee5ff61a44a3
Thomas <thomas.pellan@gmail.com>
2023-03-24 16:13:09 +0100

    feat(banner) functionnal banner with background image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import image from '@astrojs/image';
+import { astroImageTools } from "astro-imagetools";
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [vue(), image()],
+    integrations: [vue(), image(), astroImageTools],
 });
