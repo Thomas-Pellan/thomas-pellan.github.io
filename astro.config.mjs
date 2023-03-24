0129e32a92435b7c2363d1956dad6dfb8973bbb0
Thomas <thomas.pellan@gmail.com>
2023-03-24 19:54:13 +0100

    feat(section) adding parametrized sections with images, videos and text. adding compression to dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import image from '@astrojs/image';
 import { astroImageTools } from "astro-imagetools";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [vue(), image(), astroImageTools],
+  integrations: [vue(), image(), astroImageTools, compress()]
 });
