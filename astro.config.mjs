ea8d727928ebd7552657dfa56838d8425d84cbc2
Thomas <thomas.pellan@gmail.com>
2023-03-25 18:20:52 +0100

    feat(sitemap) adding sitemap with serialize rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,26 @@ import image from '@astrojs/image';
 import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
 
-// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [vue(), image(), astroImageTools, compress()]
+  site: 'https://www.ibf-equicoaching.com',
+  integrations: [vue(), image(), astroImageTools, compress(), sitemap({
+    serialize(item) {
+      item.changefreq = 'weekly';
+      item.lastmod = new Date();
+      item.priority = 0.9;
+      if (/blog/.test(item.url)) {
+        item.changefreq = 'weekly';
+        item.lastmod = new Date();
+        item.priority = 0.7;
+      }
+      if (/partners/.test(item.url) ) {
+        item.changefreq = 'monthly';
+        item.lastmod = new Date();
+        item.priority = 0.7;
+      }
+      return item;
+    },
+  }),]
 });
