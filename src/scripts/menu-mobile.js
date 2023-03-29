34813e2e81a2960cb2d143c58235e6ca5c84fbf2
Thomas <thomas.pellan@gmail.com>
2023-03-29 15:32:48 +0200

    fix(lint) linter and cleaning on astro files

--- src/scripts/menu-mobile.js
+++ src/scripts/menu-mobile.js
@@ -2,23 +2,22 @@
 const icons = document.querySelectorAll('.m-header-icon')
 
 icons.forEach((icon) => {
-    if(!icon){
-        return
-    }
-    icon.addEventListener('click', () => {
-        toggleMobileMenu()
-    });
+  if (!icon) {
+    return
+  }
+  icon.addEventListener('click', () => {
+    toggleMobileMenu()
+  })
 })
 
 document.querySelector('.m-header-mobile-overlay').addEventListener('click', () => {
-    toggleMobileMenu()
-});
+  toggleMobileMenu()
+})
 
-function toggleMobileMenu (){
-    document.querySelectorAll('.m-header-icon').forEach((item) => {
-        item.classList.toggle('hidden')
-    })
-    document.querySelector('.m-header-menu').classList.toggle('show')
-    document.querySelector('.m-header-mobile-overlay').classList.toggle('hidden')
+function toggleMobileMenu () {
+  document.querySelectorAll('.m-header-icon').forEach((item) => {
+    item.classList.toggle('hidden')
+  })
+  document.querySelector('.m-header-menu').classList.toggle('show')
+  document.querySelector('.m-header-mobile-overlay').classList.toggle('hidden')
 }
-
--- src/scripts/sub-menu.js
+++ src/scripts/sub-menu.js
@@ -1,37 +1,37 @@
 const submenus = document.querySelectorAll('li.m-submenu')
 
-function toggleContent(content, arrow) {
-    content.classList.toggle('m-submenu-content-hidden')
-    arrow.classList.toggle('m-submenu-arrow-active')
-    setTimeout(() => {
-        if(!content.classList.contains('m-submenu-content-hidden')){
-            content.classList.toggle('m-submenu-content-hidden')
-            arrow.classList.toggle('m-submenu-arrow-active')
-        }
-    },10000);
+function toggleContent (content, arrow) {
+  content.classList.toggle('m-submenu-content-hidden')
+  arrow.classList.toggle('m-submenu-arrow-active')
+  setTimeout(() => {
+    if (!content.classList.contains('m-submenu-content-hidden')) {
+      content.classList.toggle('m-submenu-content-hidden')
+      arrow.classList.toggle('m-submenu-arrow-active')
+    }
+  }, 10000)
 }
 
 submenus.forEach((submenu) => {
-    if(!submenu){
-        return
-    }
-    const action = submenu.querySelector('.m-submenu-action')
-    const link = submenu.querySelector('.m-submenu-link')
-    const content = submenu.querySelector('.m-submenu-content')
-    const arrow = submenu.querySelector('.m-submenu-arrow')
-    if(!content || !arrow || !action){
-        return
-    }
-    content.addEventListener('mouseleave', () => {
-        toggleContent(content, arrow)
+  if (!submenu) {
+    return
+  }
+  const action = submenu.querySelector('.m-submenu-action')
+  const link = submenu.querySelector('.m-submenu-link')
+  const content = submenu.querySelector('.m-submenu-content')
+  const arrow = submenu.querySelector('.m-submenu-arrow')
+  if (!content || !arrow || !action) {
+    return
+  }
+  content.addEventListener('mouseleave', () => {
+    toggleContent(content, arrow)
+  })
+  const actions = ['mouseover']
+  if (!link) {
+    actions.push('click')
+  }
+  actions.forEach(function (e) {
+    action.addEventListener(e, () => {
+      toggleContent(content, arrow)
     })
-    let actions = ['mouseover']
-    if(!link){
-        actions.push('click')
-    }
-    actions.forEach(function(e){
-        action.addEventListener(e, () => {
-            toggleContent(content, arrow)
-        })
-    })
+  })
 })
