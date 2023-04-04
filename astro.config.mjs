2eefb2edc7bd4121c30cc30d2faa9af4809ed521
thomas <thomas.pellan@gmail.com>
2023-04-04 14:03:41 +0200

    fix(svg) using local svg files to avoid astro-icon fail at build, dockerfile to test locally generated website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import vue from '@astrojs/vue';
 import image from '@astrojs/image';
 import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), image(), astroImageTools, compress()]
+  site: 'https://www.ibf-equicoqching.com',
+  integrations: [vue(), image(), astroImageTools, compress(), sitemap()],
 });
