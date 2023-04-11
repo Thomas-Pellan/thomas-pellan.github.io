228dfd5e1f465877f0bc441df056f72fcc970f3e
thomas <thomas.pellan@gmail.com>
2023-04-11 21:34:05 +0200

    fix(base) fixing base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thomas-pellan.github.io',
-  base: '/ibf-front',
   integrations: [vue(), image(), astroImageTools, compress(), sitemap({
     serialize(item) {
       item.changefreq = 'weekly';
