51b7ef6fca9ba7f2babdc6bb3a705f795bf7a719
Thomas <thomas.pellan@gmail.com>
2023-04-25 11:42:06 +0200

    feat(contact) common scss and component skeleton

--- src/class/Prospect.ts
+++ src/class/Prospect.ts
@@ -3,23 +3,18 @@ class Prospect {
     private _mail: string;
     private _phone: string;
     private _dataConsent: boolean;
-
     constructor(name: string, mail: string, phone: string, dataConsent: boolean) {
         this._name = name;
         this._mail = mail;
         this._phone = phone;
         this._dataConsent = dataConsent;
     }
-
     get dataConsent(): boolean {
         return this._dataConsent;
     }
-
     set dataConsent(value: boolean) {
         this._dataConsent = value;
     }
-
-
     get name(): string {
         return this._name;
     }
---
+++ src/components/vue/ContactForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+            id="contact-form"
+            class="m-event-form"
+            aria-label="Contactez moi"
+    >
+        
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'ContactForm',
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
