52636118ce9fa4927167d0a21697787994b34855
Thomas <thomas.pellan@gmail.com>
2023-05-19 19:15:39 +0200

    feat(partners) adding kare events page

--- src/class/Image.ts
+++ src/class/Image.ts
@@ -2,9 +2,24 @@ class Image {
 
     private readonly _src: string;
     private readonly _alt: string;
-    constructor(src: string, alt: string) {
+    private readonly _width: number = 320;
+    private readonly _height: number = 320;
+
+    constructor(src: string, alt: string, width: number | null, height: number | null) {
         this._src = src;
         this._alt = alt;
+        if(width)
+            this._width = width;
+        if(height)
+            this._height = height;
+    }
+
+    get width(): string {
+        return this._width;
+    }
+
+    get height(): string {
+        return this._height;
     }
 
     get src(): string {
