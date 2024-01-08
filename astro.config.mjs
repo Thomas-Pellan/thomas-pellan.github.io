2a5ab287511fcb695a2537f692d7d141d540a21e
Thomas <thomas.pellan@gmail.com>
2024-01-08 12:44:54 +0100

    adding sitemap videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,26 @@ export default defineConfig({
         item.lastmod = new Date();
         item.priority = 0.7;
       }
+      if ('https://www.ibf-equicoaching.com/' === item.url) {
+        item.video = [{
+          title: 'Et si vous osiez le coaching autrement ? IBF Equicoaching',
+          description: 'Présentation de IBF Equicoaching',
+          thumbnail_loc: 'https://i.ytimg.com/vi/pgN6-mk2y4A/hqdefault.jpg',
+          content_loc: 'https://www.youtube.com/watch?v=pgN6-mk2y4A',
+          duration: 209,
+          allow_embed: 'yes'
+        }]
+      }
+      if ('https://www.ibf-equicoaching.com/about' === item.url) {
+        item.video = [{
+          title: 'Camille Marzec, Équicoach certifiée et fondatrice d\'IBF Équicoaching - Coach Station',
+          description: 'Interview IBF Equicoaching',
+          thumbnail_loc: 'https://i.ytimg.com/vi/qZ4Y1D2HMi4/hqdefault.jpg',
+          content_loc: 'https://www.youtube.com/watch?v=qZ4Y1D2HMi4',
+          duration: 3036,
+          allow_embed: 'yes'
+        }]
+      }
       //Removing trailling slash from sitemap urls
       if(item.url.slice(-1) === '/'){
         item.url = item.url.slice(0, -1)
